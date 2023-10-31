50ca537f4b625771984ee96d2dcbf4b0ff3f797e
MalTok <mig01@wp.pl>
2023-10-31 11:36:01 +0100

    Utworzenie zadania

---
+++ src/main/java/pl/mt/task/Zadanie221Application.java
@@ -0,0 +1,13 @@
+package pl.mt.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zadanie221Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Zadanie221Application.class, args);
+    }
+
+}
---
+++ src/test/java/pl/mt/task/Zadanie221ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mt.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Zadanie221ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
